3cfb02fb76679eeaa086b68ffd09987e10e98d66
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-08-26 18:46:40 +0200

    Added dataFiles. Polished  printResult method

--- src/MapGraph.java
+++ src/MapGraph.java
@@ -23,7 +23,7 @@ public class MapGraph {
         }
     }
 
-    public void AStar(Node startingNode, Node goalNode, String outputFile, boolean djikstra) throws Exception{
+    public void FindShortestPath(Node startingNode, Node goalNode, String outputFileName, boolean djikstra) throws Exception{
 
 
         Date start = new Date();
@@ -51,21 +51,14 @@ public class MapGraph {
 
             iteration++;
         }
-        
-        //TODO: Actually create the second stage and end-printout :P
-        //System.out.println("Moving on to second stage!");
-
-        //System.out.println(goalNode.getPreviousNode());
 
         slutt = new Date();
         System.out.println("Time before print: " + (double) (slutt.getTime()-start.getTime()) + "ms");
 
-        printResult(startingNode, goalNode, outputFile);
+        printResult(startingNode, goalNode, outputFileName);
     }
 
     private void expandNode(Node node, Node goalNode, PriorityQueue<Node> priorityQueue, boolean djikstra){
-
-        //System.out.println("Expanding: " + node);
 
         node.setExpanded(true);
 
@@ -85,12 +78,11 @@ public class MapGraph {
                 edge.getToNode().setPriority(edge.getToNode().getCost());
 
             }else {
-                if (edge.getToNode().hasDirectdistanceCalculated()){
-                    edge.getToNode().setPriority((int)(edge.getToNode().getDirectDistance() / 130 * 360000) /*Converts km to seconds of travel time*/ + edge.getToNode().getCost());
-                }else {
+                /*Converts km to seconds of travel time*/
+                if (!edge.getToNode().hasDirectdistanceCalculated()) {
                     edge.getToNode().calculateDirectDistanceToNode(goalNode);
-                    edge.getToNode().setPriority((int)(edge.getToNode().getDirectDistance() / 130 * 360000) /*Converts km to seconds of travel time*/ + edge.getToNode().getCost());
                 }
+                edge.getToNode().setPriority((int)(edge.getToNode().getDirectDistance() / 130 * 360000) /*Converts km to seconds of travel time*/ + edge.getToNode().getCost());
             }
 
 
@@ -105,11 +97,21 @@ public class MapGraph {
         }
     }
 
-    private void printResult(Node startingNode, Node goalNode, String outputFile) throws Exception{
+    /**
+     * Method that prints the result of a shortest path algorithm to two files.
+     * The first is a .txt file containing travel time, distance, amount of nodes visited, and a list of lat/long for all the visited nodes.
+     * The second is a .csv file containing the lat/long of all the nodes visited. The name of the .csv file is outputFileName_nodesOnly.csv.
+     *
+     * @param startingNode The location where the search started.
+     * @param goalNode The destination of the search.
+     * @param outputFileName The name (without extensions) of the output files.
+     * @throws Exception
+     */
+    private void printResult(Node startingNode, Node goalNode, String outputFileName) throws Exception{
         Node currentNode = goalNode;
         Node prevNode;
 
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFileName + ".txt"));
 
         int totalTime = goalNode.getCost(); // Time in seconds
         int totalDistance = 0;
@@ -120,8 +122,6 @@ public class MapGraph {
         while (currentNode != startingNode){
 
             nodePathList.add(currentNode);
-
-            //System.out.println("Adding node: " + currentNode.getNodeNum());
 
             prevNode = currentNode.getPreviousNode();
 
@@ -144,11 +144,32 @@ public class MapGraph {
         int minutes = (totalTime % 360000) / 6000;
         int seconds = (totalTime % 6000) / 100;
 
-        bufferedWriter.write(Integer.toString(hours) + "t " + Integer.toString(minutes) + "m " + Integer.toString(seconds) + "s");
+        bufferedWriter.write("Travel time");
         bufferedWriter.newLine();
-        bufferedWriter.write(Integer.toString(totalDistance / 1000) + "km");
+        bufferedWriter.write(hours + "t " + minutes + "m " + seconds + "s");
         bufferedWriter.newLine();
-        bufferedWriter.write("Nodes traversed: " + Integer.toString(amountOfNodes));
+        bufferedWriter.write("Distance");
+        bufferedWriter.newLine();
+        bufferedWriter.write(totalDistance / 1000 + "km");
+        bufferedWriter.newLine();
+        bufferedWriter.write("Nodes traversed: " + amountOfNodes);
+        bufferedWriter.newLine();
+        bufferedWriter.newLine();
+        bufferedWriter.write("latitude,longitude");
+        bufferedWriter.newLine();
+
+        for (int i = nodePathList.size() - 1; i >= 0; i--) {
+
+            bufferedWriter.write(nodePathList.get(i).getLatitude() + "," + nodePathList.get(i).getLongitude());
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        /* Print CSV with the nodes only */
+        bufferedWriter = new BufferedWriter(new FileWriter(outputFileName + "_nodesOnly.csv"));
+
+        bufferedWriter.write("latitude,longitude");
         bufferedWriter.newLine();
 
         for (int i = nodePathList.size() - 1; i >= 0; i--) {
@@ -162,21 +183,30 @@ public class MapGraph {
 
     public static void main(String[] args) throws Exception{
 
+        int fromNode = 2460904; // Trondheim
+        int toNode = 2419175; // Oslo
+        String nodeFilepath = "nodesScandinavia.txt";
+        String edgeFilepath = "edgesScandinavia.txt";
+
         MapGraph mapGraph = new MapGraph();
         Loader loader = new Loader();
 
-        Node[] nodeArray = loader.loadNodes("noder.txt");
+        System.out.println("Reading nodes...");
 
-        loader.loadEdges("kanter.txt", nodeArray);
+        Node[] nodeArray = loader.loadNodes("nodesScandinavia.txt");
 
-        System.out.println("Beginning program");
+        System.out.println("Reading edges...");
+
+        loader.loadEdges("edgesScandinavia.txt", nodeArray);
+
+        System.out.println("Beginning program...");
 
         System.out.println("Djikstra");
-        mapGraph.AStar(nodeArray[2617841], nodeArray[1491126], "outputDjikstra.txt", true);
+        mapGraph.FindShortestPath(nodeArray[fromNode], nodeArray[toNode], "outputDjikstra", true);
 
         mapGraph.reset(nodeArray);
 
         System.out.println("A*");
-        mapGraph.AStar(nodeArray[2617841], nodeArray[1491126], "output.txt", false);
+        mapGraph.FindShortestPath(nodeArray[fromNode], nodeArray[toNode], "outputAStar", false);
     }
 }
